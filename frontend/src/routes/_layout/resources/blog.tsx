52dfd5feae2a95e8ef273da66a202b3c8c172230
cobalt admin <nik@cobaltdata.net>
2025-07-22 06:04:43 +0000

    blog-ad

--- frontend/src/routes/_layout/resources/blog.tsx
+++ frontend/src/routes/_layout/resources/blog.tsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect } from 'react';
 import { Box, Flex, Grid, Heading, Text, VStack, Button, Link, SimpleGrid, Image, Tag, HStack, Divider, Badge, Spinner } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
@@ -73,6 +74,11 @@ function BlogPage() {
 
   return (
     <>
+      <Box width="100%" textAlign="center" py={4}>
+        <a href="https://www.dpbolvw.net/click-101487435-17085984" target="_top">
+          <img src="https://www.tqlkg.com/image-101487435-17085984" width="510" height="210" alt="" border="0"/>
+        </a>
+      </Box>
       <Box py={16} bg="red.50" color="gray.800">
         <Box maxW="1200px" mx="auto" px={4}>
           <Heading as="h1" size="2xl" textAlign="center" mb={6} fontWeight="medium">Guides and Trends</Heading>
